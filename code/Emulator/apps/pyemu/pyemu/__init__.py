05d8b808a49200c66cbbeba77767ec6c66ccc005
Vytautas Astrauskas <vastrauskas@gmail.com>
2011-03-08 11:53:33 +0200

    Patvarkytas GUI šablonas.

--- code/Emulator/apps/pyemu/pyemu/__init__.py
+++ code/Emulator/apps/pyemu/pyemu/__init__.py
@@ -14,19 +14,23 @@ def main(argv=None):
     if not argv:
         argv = sys.argv[:]
 
-
-    app = wx.App()
-    frame = wx.Frame(None, -1, 'Bla')
-    frame.Show()
-    app.MainLoop()
-
-    if len(argv) != 2:
+    gui = False
+    if len(argv) == 2:
+        file = argv[1]
+    elif len(argv) == 3 and argv[1] == 'gui':
+        gui = True
+        file = argv[2]
+    else:
         print u'Naudojimas: pyemu <failas>'
         sys.exit(1)
-    else:
-        file = argv[1]
 
     rm = RealMachine()
-    rm.load_virtual_machine(file)
-    rm.processor.execute()
 
+    if gui:
+        app = wx.App()
+        frame = wx.Frame(None, -1, 'Bla')
+        frame.Show()
+        app.MainLoop()
+    else:
+        rm.load_virtual_machine(file)
+        rm.processor.execute()
