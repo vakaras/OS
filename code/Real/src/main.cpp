82fe226ffdc8e0ecd798dfd4aed6549de47dfa6e
Vytautas Astrauskas <vastrauskas@gmail.com>
2011-05-03 14:58:28 +0300

    Pravalyta main funkcija.

--- code/Real/src/main.cpp
+++ code/Real/src/main.cpp
@@ -23,13 +23,6 @@
 Monitor monitor;
 IDT idt;
 
-extern "C" void interrupt_handler(int *stack) {
-
-  monitor.write_hex((u64int) stack);
-
-  }
-
-extern "C" void load_pager(u64int);
 
 extern "C" int main() {
 
@@ -38,6 +31,7 @@ extern "C" int main() {
   test_monitor(&monitor);
   test_idt();
 
+  // Žaidimas su puslapiavimo mechanizmu.
   u64int *pml = (u64int *) 0x0000000000103000;
   u64int *pdp1 = (u64int *) (pml[0] & PTINV);
   u64int *pd1 = (u64int *) (pdp1[0] & PTINV);
@@ -63,95 +57,6 @@ extern "C" int main() {
     monitor.write_hex(pd1[i]);
     monitor.write_string("\n");
     }
-  //initialise_paging();
-
-  //u64int *pml = (u64int *) 0x0000000000103000;
-  //u64int *pdp1 = (u64int *) (pml[0] & PTINV);
-  //u64int *pd1 = (u64int *) (pdp1[0] & PTINV);
-
-  //monitor.clear();
-
-  //monitor.write_string("PML[");
-  //monitor.write_dec(0);
-  //monitor.write_string("] \t= ");
-  //monitor.write_hex(pml[0]);
-  //monitor.write_string("\n");
-
-  //monitor.write_string("PDP[");
-  //monitor.write_dec(0);
-  //monitor.write_string("] \t= ");
-  //monitor.write_hex(pdp1[0]);
-  //monitor.write_string("\n");
-
-  //for (int i = 0; i < 10; i++) {
-    //monitor.write_string("PD[");
-    //monitor.write_dec(i);
-    //monitor.write_string("] \t= ");
-    //monitor.write_hex(pd1[i]);
-    //monitor.write_string("\n");
-    //}
-
-    
-  //u64int *p2 = (u64int *) 0xFFFF800000000000;
-  //for (int i = 0; i < 100; i++) {
-    //p2[i] = 0x0;
-    //}
-
-  //u8int *p3 = (u8int *) 0xFFFF800000000000;
-  //p3[0] = 0xAA;
-
-  //u64int *p = (u64int *) &idt;
-  //for (int i = 0; i < 10; i++) {
-    //p[i] = 0xDEADBABA + (i * 0x100000000);
-    //}
-  //pause1((u64int) &idt);
-  
-  // Atspausdina PML reikšmes.
-//   for (int i = 0; i < 10; i++) {
-//     monitor.write_string("PML4[");
-//     monitor.write_dec(i);
-//     monitor.write_string("] \t= ");
-//     monitor.write_hex(pml4[i]);
-//     monitor.write_string("\n");
-//     }
-//   for (int i = 250; i < 260; i++) {
-//     monitor.write_string("PML4[");
-//     monitor.write_dec(i);
-//     monitor.write_string("] \t= ");
-//     monitor.write_hex(pml4[i]);
-//     monitor.write_string("\n");
-//     }
-// 
-//   monitor.write_hex((u64int) pml4);
-//   monitor.write_string(" = ");
-//   monitor.write_hex(*pml4);
-// 
-//   load_pager(0xFFFFFFFFFF103000);
-//   load_pager(0x0000000000103000);
-
-  //gdt.print_debug_info(&monitor);
-
-  //asm volatile ("int $0x3");
-  
-  monitor.write_string("IDT address:         \t");
-  monitor.write_hex((u64int) &idt);
-  monitor.write_string("\nFIXED IDT address: \t");
-  monitor.write_hex(FIX_ADDRESS((u64int) &idt));
-
-  //gdt.print_debug_info(&monitor);
-  //idt.print_debug_info(&monitor);
-  u64int a, b;
-  a = 23;
-  b = 0;
-  b = a + b + 1;
-  idt.print_debug_info(&monitor);
-  //u64int a, b;
-  //a = 23;
-  //b = 0;
-  //b = a + b + 1;
-
-  pause();
-  asm volatile("int $0x4;");
 
   return 0xBABADEAD;
   }
