e36bca7b34d3da1a220389c2c381dad66ceda79f
Egidijus Lukauskas <Egils.ps@gmail.com>
2011-05-03 09:26:59 +0300

    missing files

--- code/Real/src/main.cpp
+++ code/Real/src/main.cpp
@@ -8,20 +8,24 @@
 #include "monitor_screen_character.h"
 #include "monitor.h"
 #include "gdt.h"
-// #include "idt.h"
-#include "pic.h"
+#include "idt.h"
+// #include "pic.h"
 #include "tests/test_monitor.h"
 
 
 // Globalūs kintamieji.
 Monitor monitor;
 GDT gdt;
+IDT idt;
 
 extern "C" int main() {
-  InitInterrupts();
-  test_monitor(&monitor);
-  //gdt.print_debug_info(&monitor);
-  
-  asm volatile("int $0x4;");
+//   InitInterrupts();
+  //test_monitor(&monitor);
+  gdt.print_debug_info(&monitor);
+  idt.print_debug_info(&monitor);
+  u64int a, b;
+  a = 23;
+  b = 0;
+  b = a + b + 1;
   return 0xBABADEAD;
   }
