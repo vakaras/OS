38ef4f979377f418db5301157feff2a0fc17b9f7
Vytautas Astrauskas <vastrauskas@gmail.com>
2011-05-03 15:18:38 +0300

    Šiek tiek pertvarkyti primityvai.

--- code/Real/src/main.cpp
+++ code/Real/src/main.cpp
@@ -14,10 +14,6 @@
 #include "tests/test_debug.h"
 #include "tests/test_idt.h"
 
-#define PAGE_TABLE_PERM ((1<<0) | (1<<1))
-#define PAGE_TABLE_PERM_NOCACHE (PAGE_TABLE_PERM | (1<<3) | (1<<4))
-#define PAGE_TABLE_LARGE_FLAG (1<<7)
-#define PTINV 0xFFFFFFFFFFFFF000
 
 // Globalūs kintamieji.
 Monitor monitor;
@@ -30,33 +26,6 @@ extern "C" int main() {
   test_debug();
   test_monitor(&monitor);
   test_idt();
-
-  // Žaidimas su puslapiavimo mechanizmu.
-  u64int *pml = (u64int *) 0x0000000000103000;
-  u64int *pdp1 = (u64int *) (pml[0] & PTINV);
-  u64int *pd1 = (u64int *) (pdp1[0] & PTINV);
-
-  monitor.clear();
-
-  monitor.write_string("PML[");
-  monitor.write_dec(0);
-  monitor.write_string("] \t= ");
-  monitor.write_hex(pml[0]);
-  monitor.write_string("\n");
-
-  monitor.write_string("PDP[");
-  monitor.write_dec(0);
-  monitor.write_string("] \t= ");
-  monitor.write_hex(pdp1[0]);
-  monitor.write_string("\n");
-
-  for (int i = 0; i < 10; i++) {
-    monitor.write_string("PD[");
-    monitor.write_dec(i);
-    monitor.write_string("] \t= ");
-    monitor.write_hex(pd1[i]);
-    monitor.write_string("\n");
-    }
 
   return 0xBABADEAD;
   }
--- code/Real/src/primitives.h
+++ code/Real/src/primitives.h
@@ -6,19 +6,16 @@
 
 #include "types.h"
  
+
 // Pasiųsti baitą į nurodytą portą.
 void send_byte(u16int port, u8int value);
+
 
 // Nuskaityti baitą iš nurodyto porto.
 u8int get_byte(u16int port);
 
+
 // Nuskaityti du baitus iš nurodyto porto.
 u16int get_bytes2(u16int port);
-
-// Nukopijuoja nurodytus len baitų iš src į dest.
-void memcpy(u8int *dest, const u8int *src, u32int length);
-
-// Užpildo nurodytą atmintį reikšme value.
-void memset(u8int *dest, u8int value, u32int length);
 
 #endif
